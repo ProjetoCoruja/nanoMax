d25f18f2c856074ea81f760302ade2eebf69cad8
Klauss Sant'Ana <falecom@klaussantana.com>
2012-09-11 20:52:23 -0300

    Primeiro Commit
    Adicionando os arquivos já prontos do framework.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * nanoMax framework
+ *
+ * Um framework para desenvolvimento de aplicações desenvolvido para PHP 5.3 ou mais recente.
+ *
+ * @package      nanoMax
+ * @author       Klauss Sant'Ana Guimarães
+ * @copyright    Copyright (c) klaussantana.com
+ * @license      http://www.gnu.org/licenses/lgpl.html LGPL - LESSER GENERAL PUBLIC LICENSE
+ * @link         http://nanoMax.klaussantana.com
+ * @version      0.1-dev
+ * @filesource   
+ **/
+
+require 'core/bootstrap.php';
+
+?>
